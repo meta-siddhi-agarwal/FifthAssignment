e739e64d3edb8c88017eeefbdd1cad6ee6bf58ba
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-03-18 10:39:24 +0530

    LcmHcf.java

--- src/pac/LcmHcf.java
+++ src/pac/LcmHcf.java
@@ -1,52 +1,108 @@
 package pac;
 
-import java.lang.invoke.MethodHandles.Lookup.ClassOption;
 import java.util.Scanner;
 
 public class LcmHcf {
 
+	//class obj. for calling non-static functions of class
 	static LcmHcf classObject=new LcmHcf();
 	
+	//variable will control the loop
+	static boolean isValid=true;
+	
+	//numbers which will be taken as input from user
 	static int firstNumber,secondNumber;
 	
+	//scanner obj. for taking input from user
 	static Scanner scannerObject=new Scanner(System.in);
+	
 	public static void main(String[] args) {
-    firstNumber=classObject.inputFromUser();
-    secondNumber=classObject.inputFromUser();
-    if(firstNumber<secondNumber) {
-    	System.out.println("HCF of two no. is "+(classObject.computeHcf(firstNumber, secondNumber, firstNumber)));
-    }
-    else System.out.println("HCF of two no. is "+(classObject.computeHcf(firstNumber, secondNumber, secondNumber)));
-    
-    if(firstNumber<secondNumber) {
-    	 System.out.println("LCM of two no. is "+(classObject.computeLcm(firstNumber, secondNumber, 1, 2, firstNumber)));
-    }
-    else {
-    	System.out.println("dasd");
-    	System.out.println("LCM of two no. is "+(classObject.computeLcm(firstNumber, secondNumber, 1, 2, secondNumber)));
-    }
-    
+		while(isValid) {
+			try {
+			    System.out.println("Which operation you want to perform \n"+
+			    "1.HCF\n"+"2.LCM\n"+"3.Exit");
+			    int input=scannerObject.nextInt();
+			    switch(input) {
+			    
+			    //case 1 will compute hcf
+			    case 1:
+			    firstNumber=classObject.inputFromUser();
+				secondNumber=classObject.inputFromUser();
+			    if(firstNumber<secondNumber) {
+			    	System.out.println("HCF of two no. is "+
+			    (classObject.computeHcf(firstNumber, secondNumber, firstNumber)));
+			    }
+			    else System.out.println("HCF of two no. is "+
+			    (classObject.computeHcf(firstNumber, secondNumber, secondNumber)));
+			    break;
+			    
+			    //case 2 will compute lcm
+			    case 2:
+			    firstNumber=classObject.inputFromUser();
+				secondNumber=classObject.inputFromUser();			    
+			    System.out.println("LCM of two no. is "+
+				(classObject.computeLcm(firstNumber, secondNumber, 1, 2)));
+			    break;
+			    
+			    //case 3will terminate the program
+			    case 3:
+			    	System.out.println("Program ended");
+			    	isValid=false;
+			    	
+			    //default case in case user enters no. other than provided option
+			    default:
+			    	System.out.println("Enter valid no.");
+	
+			    }
+			}
+			catch(Exception e) {
+				System.out.println("Enter valid input");
+				scannerObject.nextLine();
+			}			
+		}    
 	}
 	
+	/**
+	 * will take a number as input from user
+	 * @return number entered be the user
+	 */
 	int inputFromUser() {
 		System.out.println("Enter a no.");
 		int number=scannerObject.nextInt();
 		return number;
 		
 	}
- int computeHcf(int number1,int number2,int commonFactors) {
-	if(number1%commonFactors==0 && number2%commonFactors==0)return commonFactors;
-	return computeHcf(number1, number2, commonFactors-1);
+	
+	/**
+	 * will compute hcf of two numbers
+	 * @param firstNumber which is entered be the user
+	 * @param secondNumber which is entered be the user
+	 * @param commonFactors which will depend on either first number is smaller 
+	 * or second number
+	 * @return hcf of the two numbers
+	 */
+ int computeHcf(int firstNumber,int secondNumber,int commonFactors) {
+	if(firstNumber%commonFactors==0 && secondNumber%commonFactors==0)return commonFactors;
+	return computeHcf(firstNumber, secondNumber, commonFactors-1);
 }
+    /**
+     * will compute lcm of two numbers
+     * @param firstNumber which is entered be the user
+     * @param secondNumber which is entered be the user
+     * @param product which is initially 1
+     * @param commonFactors which is initially 2
+     * @return lcm of two numbers
+     */
  
- int computeLcm(int number1,int number2,int product,int commonFactors,int smallestNumber) {
-	 if(commonFactors>smallestNumber)   return product;
+ int computeLcm(int firstNumber,int secondNumber,int product,int commonFactors) {
+	 if(commonFactors>firstNumber && commonFactors>secondNumber)   return product;
 	 
-	 if((number1%commonFactors==0) && (number2%commonFactors==0)) {
+	 if((firstNumber%commonFactors==0) || (secondNumber%commonFactors==0)) {
 		 product*=commonFactors;
-		 number1/=commonFactors;
-		 number2/=commonFactors;
+		 if(firstNumber % commonFactors==0) firstNumber/=commonFactors;
+		 if(secondNumber % commonFactors==0) secondNumber/=commonFactors;
+		 return computeLcm(firstNumber, secondNumber, product, commonFactors);
 	 }
-	 return computeLcm(number1, number2, product, ++commonFactors, smallestNumber);
+	 else  return computeLcm(firstNumber, secondNumber, product, ++commonFactors);
  }
 }
